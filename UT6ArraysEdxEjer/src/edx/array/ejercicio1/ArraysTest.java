521acc7b50a7a8ec6a52afcca6ea0d5a3916a485
Raquel <raquelsp@educastur.org>
2019-03-17 21:57:20 +0100

    Archivo Inicial para Tarea Evaluable Arrays

---
+++ UT6ArraysEdxEjer/src/edx/array/ejercicio1/ArraysTest.java
@@ -0,0 +1,67 @@
+package edx.array.ejercicio1;
+
+/*Operaciones con Arrays 
+
+Implementamos dos métodos, uno para encontrar el índice donde se encuentra 
+el mínimo de un array de enteros, y otro para encontrar el valor mínimo del array. 
+
+Compila y ejecuta el código proporcionado. 
+Como puedes ver, hay errores dentro de estos métodos. Por favor, encuéntralos y corrige el código.
+
+Hay un método dentro de la clase java.util.Arrays que puede sustituir a nuestro método print(). ¿Cuál es el nombre de este método?
+*/
+
+public class ArraysTest {
+
+	   private static void print(String name, int a[]){
+		    System.out.print(name +" = [");
+		    for (int i=0; i < a.length-1; i++){
+		        System.out.print(a[i]+", ");
+		    }
+		    System.out.println(a[a.length-1]+"]");
+	    }
+
+	    private static int indexMin(int[] array) {
+	    	int var = 0;
+		    for (int i = 1; i < array.length -1; i++) {
+		        if (array[i] < var) {
+			        var = i;
+		        }
+		    }
+		    return var;
+	    }
+
+	    private static int min(int[] array) {
+		    int var = array[0];
+		    for (int i = 1; i < array.length -1; i++) {
+		        if (array[i] < var) {
+			        var = i;
+		        }
+		    }
+		    return var;
+	    }
+
+	    public static void main(String args[]){
+		    int[] arrayA = new int[]{0,1,2,3,4,5};
+	    	int[] arrayB = new int[10];
+		
+		    for (int i = 0; i<10; i++){
+		        arrayB[i] = (int) (Math.random()*100);
+		    }
+
+		    print("A", arrayA);
+		    // Complete this sentence. It should behave exactly the same as the previous method call
+		    //System.out.println("A = "+??????????);
+	    	print("B", arrayB);
+		    //System.out.println("B = "+?????????);
+		
+		    System.out.println("min(A) = "+ min(arrayA));
+		    System.out.println("indexMin(A) = "+ indexMin(arrayA));
+
+		    System.out.println("min(B) = "+ min(arrayB));
+		    System.out.println("indexMin(B) = "+ indexMin(arrayB));
+	    }
+}
+
+
+
